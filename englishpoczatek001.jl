07ee6b3a1c5cc7ae0933e8427b54354f9ffc61f9
Nauka Angielski potem Chiński <118378454+NaukaAngielski@users.noreply.github.com>
2022-11-23 11:23:12 +0100

    Add files via upload

---
+++ englishpoczatek001.jl
@@ -0,0 +1,158 @@
+evidence - dowód, dane
+passage - przejście
+evidence - dowód, dane
+threw - rzucił
+demanded - zażądał 
+demanded - zażądał
+demanded - zażądał
+forswore - wyrzekł się 
+forswore - wyrzekł się 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+unforgivable - nieprzebaczalne 
+unforgivable - nieprzebaczalne 
+unforgivable - nieprzebaczalne 
+unforgivable - nieprzebaczalne 
+unforgivable - nieprzebaczalne 
+UNFORGIVABLE - NIEPRZEBACZALNE 
+EVIDENCE - DOWÓD, dane
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+DEMANDED - zażądał
+demanded - zażądał
+demanded - zażądał 
+demanded - zażądał
+demanded - zażądał 
+demanded - zażądał 
+demanded - zażądał
+demanded - zażadał
+demanded - zażądał
+forswore - wyrzekł sie 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+FORSWORE - WYRZEKŁ SIĘ 
+PASSAGE - PRZEJŚCIE 
+PASSAGE - PRZEJŚCIE 
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+evidence - dowód, dane 
+evidence - dowód, dane 
+canopy - daszek  *
+thick - gruby
+assume - założyć
+disclosure - ujawnienie 
+helpless - bezradny
+blushoopers - bluźniercy
+decline - spadek 
+hustle - pośpiech
+bustle - gwar
+laden - obciążony, obładowany
+rattle - grzechot, grzochotka
+trolleys - wózki
+accusingly - oskarżycielsko 
+pouring - zsyp, oblanie
+sharp suits - ostre garnitury
+nose-stealing - kradnący nos
+wheezes - świszczący oddech
+slighty - nieznacznie, nieco
+garlic - czosnek
+giggles - chichot
+lame - kiepski 
+certainly - z pewnością
+apart from - oprócz, abstrachując od
+heir - dziedzic
+putting it mildly - delikatnie mówiąc
+confectionery - cukiernia 
+compartment - przedział 
+contrary - przeciwnie 
+evidence - dowód, dane 
+ACCUSINGLY - OSKARŻYCIELSKO 
+ACCUSINGLY - OSKARŻYCIELKSO 
+ACCUSINGLY - OSKARŻYCIELSKO 
+ACCUSINGLY - OSKARŻYCIELSKO 
+ACCUSINGLY - OSKARŻYCIELSKO
+ACCUSINGLY - OSKARŻYCIELSKO 
+evidence - dowód, dane
+evidence - dowód, dane
+evidence - dowód, dane
+evidence - dowód, dane 
+evidence - dowod, dane 
+evidence - dowód, dane 
+evidence - dowód, dane 
+evidence - dowód, dane 
+compartment - przedział 
+compartment - przedział 
+compartment - przedział 
+compartment - przedział
+compartment - przedział 
+compartment - przedział 
+compartment - przedział 
+compartment - przedział 
+compartment - przedział 
+compartment - przedział
+compartment - przedział 
+compartment - przedział
+compartment - przedział
+compartment - przedział
+compartment - przedział 
+compartment - przedział
+comaprtment - przedział 
+compartment - przedział 
+COMPARTMENT - przedział
+COMPARTMENT - PRZEDZIAŁ 
+COMPARTMENT - PRZEDZIAŁ 
+COMPARTMENT - PRZEDZIAŁ
+COMPARTMENT - PRZEDZIAŁ
+GIGGLES - CHICHOT 
+GIGGLES - CHOCHOT 
+GIGGLES - CHICHOT 
+GIGGLES - CHICHOT
+GIGGLES - CHICHOT
+GIGGLES - CHICHOT 
+giggles - chichot 
+giggles - chichot
+giggles - chichot
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc
+putting it mildly - delikatnie mówiąc 
+PUTTING IT MILDLY - DELIKATNIE MOWIĄC
+PUTTING IT MILDLY - DELIKATNIE mówiąc
+demanded - zażądał 
+demanded - zażądał
+demanded - zażądał 
+demanded - zażądał 
+demanded - zażądał 
+demanded - zażądał
+demanded - zażądał 
+forswore - wyrzekł się 
+EVIDENCE - DOWÓD, DANE 
+EVIDENCE - DOWÓD, DANE 
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC 
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC 
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC 
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+PUTTING IT MILDLY - DELIKATNIE MÓWIĄC
+slighty - nieznacznie, nieco 
+slighty - nieznacznie, nieco 
+slighty - nieznacznie, nieco 
+slighty - nieznacznie, nieco
+slighty - nieznacznie, nieco 
+slighty - nieznacznie, nieco 
